052137740897fde2c063b78834c4bd14b7dcefd7
AdroInfoTc <110378598+AdroInfoTc@users.noreply.github.com>
2022-08-01 13:51:23 +0530

    Add files via upload

---
+++ ServiceBuilder.kt
@@ -0,0 +1,49 @@
+package com.lookuptalk.retrofitservice
+
+import android.content.Context
+import easyop.app.repository.retrofitservice.AuthenticationInterceptor
+import okhttp3.ConnectionPool
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ServiceBuilder(val context: Context) {
+
+
+    private var retrofitApi: Retrofit? = null
+    private var retrofitMap:Retrofit? = null
+    private val interceptor = run {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    fun getRetrofit(): Retrofit? {
+        if (retrofitApi == null) {
+            val builder = OkHttpClient.Builder()
+            val okHttpClient =
+                builder.addInterceptor(AuthenticationInterceptor(context)).addNetworkInterceptor(
+                    interceptor
+                ).connectTimeout(5, TimeUnit.MINUTES)
+                    .readTimeout(5, TimeUnit.MINUTES).writeTimeout(5, TimeUnit.MINUTES)
+                    .connectionPool(
+                        ConnectionPool(0, 1, TimeUnit.SECONDS)
+                    ).retryOnConnectionFailure(true).build()
+
+//            val BASE_URL = "http://15.207.44.182:3000/"
+            retrofitApi = Retrofit.Builder()
+                .baseUrl("https://services.eazyop.in/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+        }
+        return retrofitApi
+    }
+
+
+}
+
+
